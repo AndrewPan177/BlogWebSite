814ea06e5b33c46b9057fa4c64939928bae60e73
Andrew <43976908+AndrewPan177@users.noreply.github.com>
2019-03-27 23:36:04 +0800

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class BlogArticles(models.Model):
+    title=models.CharField(max_length=30)
+    author=models.ForeignKey(User,related_name="blog_posts",on_delete=models.CASCADE)
+    body=models.TextField()
+    publish=models.DateField(default=timezone.now)
+
+    class Meta:
+        ordering=("id",)
+
+    def __str__(self):
+        return self.title
+
